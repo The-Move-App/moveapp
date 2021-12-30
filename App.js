9cd86f7be24a3c07eee17f644409f11ac4fe0b22
Said Saidahmed <ssaidahm@gmu.edu>
2021-12-30 12:18:06 -0500

    Second Push

--- App.js
+++ App.js
@@ -1,86 +1,20 @@
-import React from 'react'
-import { Platform } from 'react-native'
-import { Icon } from 'react-native-elements'
-import { StackNavigator, TabNavigator } from 'react-navigation'
-import PropTypes from 'prop-types'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import Profile1 from '/Users/saidahmed/Documents/mobile-app/MoveApp/index/screens'
-import Options from '/Users/saidahmed/Documents/mobile-app/MoveApp/index/screens/options.js'
-
-const Profile1Stack = StackNavigator(
-  {
-    profile: {
-      screen: Profile1,
-      path: '/',
-    },
-    options: {
-      screen: Options,
-      path: '/',
-    },
-  },
-  {
-    mode: 'card',
-  }
-)
-
-
-const HomeIcon = ({ focused, tintColor }) => (
-  <Icon
-    name="circle"
-    type="entypo"
-    size={26}
-    color={focused ? tintColor : 'gray'}
-  />
-)
-
-const RootTabs = TabNavigator(
-  {
-    profile1: {
-      screen: Profile1Stack,
-      navigationOptions: {
-        tabBarLabel: 'Profile1',
-        tabBarIcon: HomeIcon,
-      },
-    },
-
-    // PRODUCT
-    product1: {
-      screen: Product1Stack,
-      navigationOptions: {
-        tabBarLabel: 'Profile1',
-        tabBarIcon: HomeIcon,
-        tabBarVisible: false,
-      },
-    },
-  },
-  {
-    tabBarOptions: {
-      activeTintColor: Platform.OS === 'ios' ? 'black' : 'gray',
-      showLabel: false,
-      showIcon: true,
-      indicatorStyle: {
-        backgroundColor: 'transparent',
-      },
-      labelStyle: {
-        fontSize: 12,e
-      },
-      iconStyle: {
-        width: 30,
-        height: 30,
-      },
-      style: {
-        backgroundColor: 'white',
-        justifyContent: 'center',
-      },
-    },
-    tabBarPosition: 'bottom',
-    initialRouteName: 'profile1',
-  }
-)
-
-HomeIcon.propTypes = {
-  focused: PropTypes.bool.isRequired,
-  tintColor: PropTypes.string.isRequired,
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>The Move</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default RootTabs
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
